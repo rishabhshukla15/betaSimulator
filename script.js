d30a90e099fc5ef82afd286f7c8248078ef83adb
HarshDhussa <46370093+HarshDhussa1008@users.noreply.github.com>
2020-08-19 10:01:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function recieve(){
     decmsg=pow(encmsg,d)%N; 
     document.getElementById('decmsg').innerHTML=decmsg;
     document.getElementById('sending').style.display='none';    
-    document.getElementById('gif').innerHTML='<img src="decryption.gif" width="850px" height="350px" style="background: transparent;">';
+    document.getElementById('gif').innerHTML='<img src="decryption.gif" width="950px" height="400px" style="background: transparent;">';
     document.getElementById('decrypted').style.display='block';
 }
 
@@ -78,4 +78,30 @@ function pow(a,b){
         s=s*p;
     }      
     return s;    
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
